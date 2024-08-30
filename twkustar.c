05a6c491d653d49f962a778b490483064a5e7ed0
Thad Kustarz <twkustar@colossus.it.mtu.edu>
2024-08-30 15:30:01 -0400

    commit it

---
+++ twkustar.c
@@ -0,0 +1,15 @@
+/*
+ * CS1111 Lab1
+ * Author: Thaddeus Kustarz
+ * Email: Twkustar@mtu.edu
+ * 8/30/2024
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, Thaddeus Kustarz! \n");
+	return 0;
+}
