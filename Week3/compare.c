d315d9ee10a2065557e3348ecf3937cca5759b6e
ThatOneDegenerate <iranstadler@gmail.com>
2024-09-13 18:14:07 -0400

    compare.c

--- Week3/compare.c
+++ Week3/compare.c
@@ -1,47 +1,44 @@
 /*
- * CS1111 Compare.c
- * Author: Thad Kustarz
- * Email: twkustar@mtu.edu
+ * CS1111 Lab 2
+ * Author: Ian Ranstadler
+ * Email: imransta@mtu.edu
  * 9/13/24
  */
 
-#include <stdio.h>
-//sets up the progam  
+#include <stdio.h> 
 
 int main()
 
 {
-	// Asks for two numbers to be entered then it reads the two numbers that were entered.
+	
 	float num1, num2;
 
 	
-	printf("Enter two numbers: \n");
-	scanf("%f%f", &num1, &num2);
+	printf("Enter two numbers: \n");//Asks for two numbers from user
+	scanf("%f%f", &num1, &num2);//Reads and saves those two variables
 
-	// Checks to see if the numbers are equal or not equal then displays yes or no based off the statement
-	if ( num1 == num2 )
+	if ( num1 == num2 )//Checks if the two numbers are equal
 	{ 
-		printf("Are the numbers equal? Yes \nAre the numbers not equal? No\n");
+		printf("Are the numbers equal? Yes \nAre the numbers not equal? No\n");//If they are, this message is displayed
 	}
 	else
 	{
-		printf("Are the numbers equal? No \nAre the numbers not equal? Yes\n");
+		printf("Are the numbers equal? No \nAre the numbers not equal? Yes\n");//If not, this message is displayed
 	
 	}
 
 
-	//checks to see what number is bigger or smaller then it displays yes or no to answer the questions
-
-	  if ( num1 > num2 && num1 != num2)
+	
+	  if ( num1 > num2 && num1 != num2)//Checks if the first number is greater than or equal to the second number
         {
                 printf("Is the first number less than the second? No\nIs the first number greater than the second? Yes\nIs the first number less than or equal to the second? No\nIs the first number greater than or equal to the second? Yes\n");
         }
-        else if ( num1 < num2 && num1 != num2)
+        else if ( num1 < num2 && num1 != num2)//Checks if the first number is less than or equal to the second number 
                 {
                         printf("Is the first number less than the second? Yes\nIs the first number greater than the second? No\nIs the first number less than or equal to the second? Yes\nIs the first number greater than or equal to the second? No\n");
                 }
 
-        else if ( num1 == num2 )
+        else if ( num1 == num2 )//If the previous two statements are not true then it just checks if they are equal again
                 {
                         printf("Is the first number less than the second? No\nIs the first number greater than the second? No\nIs the first number less than or equal to the second? Yes\nIs the first number greater than or equal to the second? Yes\n");
                 }
