ac905eef8184843f4a84f59083dcc569d9d32f52
Aj101-cpu <austinjo@mtu.edu>
2024-10-11 15:05:53 -0400

    Update arraymax.c

--- week7/arraymax.c
+++ week7/arraymax.c
@@ -7,6 +7,8 @@
 
 //Sets up program
 #include <stdio.h>
+
+//States a function that runs through all of the inputs of an array and outputs the highest value in the array
 int maxValue(int x[], int num)
 {
         int lg = x[0];
@@ -27,9 +29,11 @@ int maxValue(int x[], int num)
 
 int main()
 {
+        //Declare/initialize the arrays
         int a[6]={1,4,5,10,8,9};
         int b[8]={4,5,60,5,1,100,8,10};
 
+        //Output the max value in the arrays
         printf("max in array a is %d \n", maxValue(a, 6));
         printf("max in array b is %d \n", maxValue(b, 8));
 
