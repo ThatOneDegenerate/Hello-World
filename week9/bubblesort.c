b06e26253883ecb0276fdc7e8c3000225214e4f2
ClaireSchultz <claschul@mtu.edu>
2024-10-25 15:10:49 -0400

    Create bubblesort.c

---
+++ week9/bubblesort.c
@@ -0,0 +1,30 @@
+/* CS1111 Week 9 Lab bubblesort.c
+ *
+ * @authors Claire Schultz, Ian Ranstdadler,Austin Johnson, Thad Kustarz
+ * @email claschul@mtu.edu
+ * @date 10/25/2024
+*/
+        #include <stdio.h>
+//sorts arrays through bubble sort (sets of 2)
+        void bubbleSort(int array[], int size){
+//runs through to make sure in order
+        for(int j=0; j<=size; j++){
+//runs through to check through array
+                for (int i=0; i<=size-1; i++){
+//switches values if last is less than first
+                        if (array[i+1]<array[i]){
+
+                                int arraySub = array[i+1];
+                                array[i+1] = array[i];
+                                array[i] = arraySub;
+
+                                }
+                        }
+                }
+//prints out array
+        for(int k=0; k<=size; k++){
+
+                printf("%d ", array[k]);
+        }
+
+}
