60d9d6c9e4f4f540105768e89f9cb5faa26d2723
ThatOneDegenerate <iranstadler@gmail.com>
2024-11-01 15:53:41 -0400

    swap.c

--- week 10/swap.c
+++ week 10/swap.c
@@ -18,7 +18,7 @@ int main(){
     printf("a = %d, b = %d\n", a, b);
     swap(&a,&b); // Call your swap function here.
 
-    printf("a = %d, b = %d\n", a, b);
+    printf("a = %d, b = %d\n", a, b); //prints the swapped values
 }
 
 void swap(int *a, int *b){ //executes swap function
