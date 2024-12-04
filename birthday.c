30848d742fcc32c9f6e99ed31085bfd79753ca41
ThatOneDegenerate <iranstadler@gmail.com>
2024-12-03 21:39:54 -0500

    Update birthday.c

--- birthday.c
+++ birthday.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-        struct Student_t {
+        struct Student_t { //Group of variables used in this code
                 int  id;
                 char name[20];
                 char birthday[10];
@@ -17,15 +17,15 @@
 
         typedef struct Student_t Student;
 
-        void printStudents(Student students[]);
+        void printStudents(Student students[]); //This line calls the function
 
         int main(int argc, char *argv[]) {
 
-                FILE *file = NULL;
+                FILE *file = NULL; //opens the file students.txt
                 char *filename = "student.txt";
                 file = fopen(filename, "r");
 
-                if (file == NULL) {
+                if (file == NULL) { //This line checks to see if the file could be opened
                         fprintf(stderr, "This file could not be opened: %s\n", filename);
                         return -1;
                 }
@@ -36,26 +36,26 @@
         while (fscanf(file, "%d %s %s", &students[n].id, students[n].name, students[n].birthday) == 3 && n< 10) {
                 n++;
         }
-        fclose(file);
+        fclose(file); //closes the file after the program has read it
 
         printStudents(students);
         }
 
-void printStudents(Student students[]) {
-        int processed[10] = {0};
+void printStudents(Student students[]) { //The part that does the sorting and the printing
+        int processed[10] = {0}; //keeps track of the names already read
 
         for (int n = 0; n < 10; n++) {
 
-                if (processed[n] == 1){
+                if (processed[n] == 1){ //skips a name if its already been read
                         continue;
                 }
 
-                printf("\n%s", students[n].birthday);
+                printf("\n%s", students[n].birthday); //print the names in the file
 
                 for (int d = 0; d < 10; d++) {
                         if(strcmp(students[n].birthday,students[d].birthday) == 0) {
                                 processed[d] = 1;
-                                printf("%s(%d)",students[d].name, students[d].id);
+                                printf("%s(%d)",students[d].name, students[d].id); //print the names of people who share birthdays
                         }
                 }
         }
